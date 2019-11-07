d56c16573ca9cc2f06198ec27ef8e6312765e63a
Herwig <HERWIM@de.softlab.net>
2019-11-07 16:27:39 +0100

    fixes svg loading

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,17 @@ module.exports = {
                 use: ['style-loader', 'css-loader'],
             },
             {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                  'file-loader',
+                  {
+                    loader: 'image-webpack-loader',
+                    options: {
+                      bypassOnDebug: true, // webpack@1.x
+                      disable: true, // webpack@2.x and newer
+                    },
+                  },
+                ],
             },
         ]
     },
